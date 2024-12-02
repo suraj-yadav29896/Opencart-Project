365ea161b8be9fa02bf9d4e80c076015a97c6c02
suraj <suraj@LAPTOP-62C0PDQP>
2024-12-02 14:25:22 +0530

    send all project

--- com.OpenCart/src/test/java/testBase/BaseClass.java
+++ com.OpenCart/src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -10,11 +11,14 @@ import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -37,7 +41,40 @@ public class BaseClass {
 		// logger used
 		logger=LogManager.getLogger(this.getClass());
 		logger.info("***********start the browser lounching**********");
+//==========================================================================================		
+		// setup remote execution
+		if(p.getProperty("execution_env").equalsIgnoreCase(os))
+		{
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			//Operating system
+			if(os.equalsIgnoreCase("windows"))
+			{
+				capabilities.setPlatform(Platform.WIN11);
+			}
+			else if (os.equalsIgnoreCase("mac"))
+			{
+				capabilities.setPlatform(Platform.MAC);
+			}
+			else {
+				System.out.println("no matching os");return;
+			}
+			//browser
+			switch(br.toLowerCase())
+			{
+			case "chrome": capabilities.setBrowserName("chrome");break;
+			case "firefox": capabilities.setBrowserName("firefox");break;
+			case "MicrosoftEdge": capabilities.setBrowserName("MicrosoftEdge");break;
+			default : System.out.println("no matching browser");return;
+			}
+
+			driver = new RemoteWebDriver(new URL(""),capabilities);
+		} 
+//===========================================================================
+		// setup local execution
+		
 		//for cross browser testing used
+	if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+	{
 		switch(br.toLowerCase()) 
 		{
 		case "chrome" :driver =new ChromeDriver();
@@ -55,7 +92,7 @@ public class BaseClass {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		//driver.manage().deleteAllCookies();
 	}
-
+	}
 	@AfterClass(groups={"sanity","regration","master"})
 	public void tearDown()
 	{
